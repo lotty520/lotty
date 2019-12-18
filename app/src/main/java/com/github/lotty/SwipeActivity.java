3b8b466b987df81b635d5b84ba0e1bb1eed2799b
吴晗 <han.wu@tongdun.cn>
2019-12-18 12:01:45 +0800

    优化下拉刷新控件逻辑

--- app/src/main/java/com/github/lotty/SwipeActivity.java
+++ app/src/main/java/com/github/lotty/SwipeActivity.java
@@ -1,18 +1,54 @@
 package com.github.lotty;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.component.SwipeLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SwipeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static List<String> sList = new ArrayList<>();
+
+    static {
+        sList.add("风急天高猿啸哀");
+        sList.add("渚清沙白鸟飞回");
+        sList.add("无边落木萧萧下");
+        sList.add("不尽长江滚滚来");
+        sList.add("万里悲秋常作客");
+        sList.add("百年多病独登台");
+        sList.add("艰难苦恨繁霜鬓");
+        sList.add("潦倒新停浊酒杯");
+        sList.add("朝辞白帝彩云间");
+        sList.add("千里江陵一日还");
+        sList.add("两岸猿声啼不住");
+        sList.add("轻舟已过万重山");
+        sList.add("风急天高猿啸哀");
+        sList.add("渚清沙白鸟飞回");
+        sList.add("无边落木萧萧下");
+        sList.add("不尽长江滚滚来");
+        sList.add("万里悲秋常作客");
+        sList.add("百年多病独登台");
+        sList.add("艰难苦恨繁霜鬓");
+        sList.add("潦倒新停浊酒杯");
+        sList.add("朝辞白帝彩云间");
+        sList.add("千里江陵一日还");
+        sList.add("两岸猿声啼不住");
+        sList.add("轻舟已过万重山");
+    }
+
     private SwipeLayout swipeLayout;
-
-
     private SwipeLayout.SwipeListener swipeListener = new SwipeLayout.SwipeListener() {
         @Override
         public void onRefreshing() {
@@ -28,6 +64,13 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.start).setOnClickListener(this);
         findViewById(R.id.finish).setOnClickListener(this);
         swipeLayout.setSwipeListener(swipeListener);
+        View header = View.inflate(this, R.layout.header_layout, null);
+        swipeLayout.setHeader(header);
+        SwipeAdapter adapter = new SwipeAdapter(this);
+        androidx.recyclerview.widget.RecyclerView body = swipeLayout.getBody();
+        body.setAdapter(adapter);
+
+        adapter.addData(sList);
     }
 
     @Override
@@ -41,6 +84,55 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
                 break;
             default:
                 break;
+        }
+    }
+
+
+    private static class SwipeAdapter extends RecyclerView.Adapter<SwipeHolder> {
+
+        private List<String> dataList = new ArrayList<>();
+
+        private Context ctx;
+
+        public SwipeAdapter(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        public void addData(List<String> data) {
+            dataList.addAll(data);
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public SwipeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = View.inflate(ctx, R.layout.body_item_layout, null);
+            return new SwipeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SwipeHolder holder, final int position) {
+            holder.content.setText(dataList.get(position));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ctx, dataList.get(position), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
+    }
+
+    private static class SwipeHolder extends RecyclerView.ViewHolder {
+        TextView content;
+
+        public SwipeHolder(@NonNull View itemView) {
+            super(itemView);
+            content = itemView.findViewById(R.id.content);
         }
     }
 }
--- component/src/main/java/com/github/component/SwipeLayout.java
+++ component/src/main/java/com/github/component/SwipeLayout.java
@@ -13,6 +13,8 @@ import android.widget.Scroller;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * @author lotty
@@ -21,12 +23,14 @@ public class SwipeLayout extends LinearLayout {
 
     private final static int MAX_HEADER_SIZE = 400;
     private final static int MAX_FOOTER_SIZE = 200;
-    private final static int MIN_SWIPE_DIS = 5;
+    private final static int MIN_SWIPE_DIS = 10;
     private FrameLayout body;
     private FrameLayout header;
     private FrameLayout footer;
+    private RecyclerView mRecyclerView;
     private int startY;
     private int endY;
+    private boolean isBacking;
     private int headerScrolledDistance;
     private Scroller scroller = new Scroller(getContext());
 
@@ -48,14 +52,15 @@ public class SwipeLayout extends LinearLayout {
         LayoutParams headerLp = new LayoutParams(LayoutParams.MATCH_PARENT, MAX_HEADER_SIZE);
         headerLp.topMargin = -MAX_HEADER_SIZE;
         header.setLayoutParams(headerLp);
-        header.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
         header.setClickable(false);
 
         body = new FrameLayout(getContext());
         LayoutParams bodyLp = new LayoutParams(LayoutParams.MATCH_PARENT, 0);
         bodyLp.weight = 1;
         body.setLayoutParams(bodyLp);
-
+        mRecyclerView = new RecyclerView(getContext());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        body.addView(mRecyclerView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
         footer = new FrameLayout(getContext());
         LayoutParams footerLp = new LayoutParams(LayoutParams.MATCH_PARENT, MAX_FOOTER_SIZE);
@@ -100,11 +105,13 @@ public class SwipeLayout extends LinearLayout {
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.e("wh", "down");
             isPulling = true;
             endY = startY = (int) event.getRawY();
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            Log.e("wh", "up");
             isPulling = false;
             if (headerScrolledDistance == -MAX_HEADER_SIZE && !isRefreshing) {
                 isRefreshing = true;
@@ -114,37 +121,31 @@ public class SwipeLayout extends LinearLayout {
                 smoothScrollToTop();
             }
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            Log.e("wh", "move:" + isRefreshing);
             if (!isRefreshing) {
                 headerScrolledDistance = getScrollY();
                 endY = (int) event.getRawY();
                 int deltaY = endY - startY;
-                // 在起点位置下方
-                if (deltaY > 0) {
-                    if (deltaY < MAX_HEADER_SIZE) {
-                        scrollTo(0, -deltaY);
-                    } else {
-                        if (headerScrolledDistance > -MAX_HEADER_SIZE) {
-                            // 强制滑动到最大距离
-                            scrollTo(0, -MAX_HEADER_SIZE);
+                if (deltaY > MIN_SWIPE_DIS) {
+                    // 在起点位置下方
+                    if (deltaY > 0) {
+                        if (deltaY < MAX_HEADER_SIZE) {
+                            scrollTo(0, -deltaY);
+                        } else {
+                            if (headerScrolledDistance > -MAX_HEADER_SIZE) {
+                                // 强制滑动到最大距离
+                                scrollTo(0, -MAX_HEADER_SIZE);
+                            }
                         }
-                    }
-                } else {
-                    if (headerScrolledDistance < 0) {
-                        scrollTo(0, 0);
+                    } else {
+                        if (headerScrolledDistance < 0) {
+                            scrollTo(0, 0);
+                        }
                     }
                 }
             }
         }
-        return true;
-    }
-
-    @Override
-    public void computeScroll() {
-        if (!isPulling && scroller.computeScrollOffset()) {
-            scrollTo(0, scroller.getCurrY());
-            Log.e("wh", "computeScroll");
-            postInvalidate();
-        }
+        return super.dispatchTouchEvent(event);
     }
 
     private void refreshing() {
@@ -159,15 +160,37 @@ public class SwipeLayout extends LinearLayout {
         invalidate();
     }
 
+    @Override
+    public void computeScroll() {
+        if (!isPulling && isBacking && scroller.computeScrollOffset()) {
+            scrollTo(0, scroller.getCurrY());
+            if (getScrollY() == 0) {
+                isBacking = false;
+                isRefreshing = false;
+            }
+            Log.e("wh", "computeScroll");
+            postInvalidate();
+        }
+    }
+
     public void setHeader(View view) {
         header.addView(view, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 
+    public RecyclerView getBody() {
+        return mRecyclerView;
+    }
+
+    public void setBody(View view) {
+        body.removeAllViews();
+        body.addView(view, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
     public void notifySwipeFinished() {
         if (isRefreshing) {
+            isBacking = true;
             smoothScrollToTop();
             Log.e("wh", "notifySwipeFinished");
-            isRefreshing = false;
         }
     }
 
