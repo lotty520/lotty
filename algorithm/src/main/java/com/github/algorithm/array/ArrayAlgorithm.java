2245a20d7b9ff79e4e27579dac945bff96bfc8d0
吴晗 <han.wu@tongdun.cn>
2020-03-24 16:27:04 +0800

    更新部分快慢指针题目

--- algorithm/src/main/java/com/github/algorithm/array/ArrayAlgorithm.java
+++ algorithm/src/main/java/com/github/algorithm/array/ArrayAlgorithm.java
@@ -1,18 +1,34 @@
 package com.github.algorithm.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 数组相关算法
+ *
  * @author lotty
  */
 public class ArrayAlgorithm {
 
   public static void main(String[] args) {
     ArrayAlgorithm algorithm = new ArrayAlgorithm();
-    System.out.println(algorithm.pivotIndex(new int[] { -1, -1, -1, 0, 1, 1 }));
-    System.out.println(algorithm.dominantIndex(new int[] { 1, 0 }));
-    System.out.println(Arrays.toString(algorithm.plusOne(new int[] { 9 })));
+    //System.out.println(algorithm.pivotIndex(new int[] { -1, -1, -1, 0, 1, 1 }));
+    //System.out.println(algorithm.dominantIndex(new int[] { 1, 0 }));
+    //System.out.println(Arrays.toString(algorithm.plusOne(new int[] { 9 })));
+    //int[] ss = new int[100000];
+    //for (int i = 0; i < ss.length; i++) {
+    //  ss[i] = i;
+    //}
+    //long l = System.currentTimeMillis();
+    //algorithm.rotate(ss, 19987);
+    //System.out.println("耗时：" + (System.currentTimeMillis() - l));
+
+    int[] ss = new int[] {
+        1, 2, 3, 4, 4, 5, 5, 5
+    };
+    algorithm.removeDuplicates(ss);
+    System.out.println(Arrays.toString(ss));
   }
 
   /**
@@ -37,9 +53,8 @@ public class ArrayAlgorithm {
    * 特殊情况：
    * 1、数组长度为0
    * 2、如果一直到0号索引还是为9，则返回新数组：
-   *      <li>长度为 len + 1 第一位为1，其余为0</li>
-   *      <li>可以通过Arrays.copyOf(new int[] { 1 }, len + 1)完成</li>
-   *
+   * <li>长度为 len + 1 第一位为1，其余为0</li>
+   * <li>可以通过Arrays.copyOf(new int[] { 1 }, len + 1)完成</li>
    */
   public int[] plusOne(int[] digits) {
     int len = digits.length;
@@ -158,5 +173,114 @@ public class ArrayAlgorithm {
     } else {
       return -1;
     }
+  }
+
+  /**
+   * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+   *
+   * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+   *
+   * 示例 :
+   *
+   * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+   *
+   * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+   *
+   * 你不需要考虑数组中超出新长度后面的元素。
+   *
+   * 题解：
+   *
+   * 快慢指针:慢指针k = 1 , 快指针i = 1 因为第一个元素必须存在，而且只需要从第二个元素开始比较
+   *
+   * 如果num[i] != num[i-1] && i != k 则更新num[k] = num[i] , k++
+   *
+   * 最后返回k即可
+   */
+  public int removeDuplicates(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int k = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[i - 1]) {
+        if (k != i) {
+          nums[k] = nums[i];
+        }
+        k++;
+      }
+    }
+    return k;
+  }
+
+  /**
+   * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+   *
+   * 示例:
+   *
+   * 输入: [0,1,0,3,12]
+   * 输出: [1,3,12,0,0]
+   *
+   * 题解：
+   *
+   * 快慢指针 循环一次
+   *
+   * 快指针 i;
+   * 慢指针 index;
+   *
+   * i从首位开始向后移动
+   *
+   * 遇到非0时：
+   * 如果i!=index：则num[index] = num[i] , num[i] = 0 , index++
+   * 如果i==index：index++
+   *
+   * 最后非零元素都按顺序移动到数组前端，后面的数字全被置为0
+   */
+  public void moveZeroes(int[] nums) {
+    int index = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        if (i != index) {
+          nums[index] = nums[i];
+          nums[i] = 0;
+        }
+        index++;
+      }
+    }
+  }
+
+  /**
+   * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+   *
+   * 示例 :
+   *
+   * 输入: [1,2,3,4,5,6,7] 和 k = 3
+   * 输出: [5,6,7,1,2,3,4]
+   * 解释:
+   * 向右旋转 1 步: [7,1,2,3,4,5,6]
+   * 向右旋转 2 步: [6,7,1,2,3,4,5]
+   * 向右旋转 3 步: [5,6,7,1,2,3,4]
+   *
+   *
+   * 题解：
+   */
+  // TODO: 2020/3/23 该方法太耗时
+  public void rotate(int[] nums, int k) {
+    while (k > 0) {
+      for (int i = nums.length - 1; i > 0; i--) {
+        nums[i] = nums[i] + nums[i - 1];
+        nums[i - 1] = nums[i] - nums[i - 1];
+        nums[i] = nums[i] - nums[i - 1];
+      }
+      k--;
+    }
+  }
+
+  public List<Integer> getRow(int rowIndex) {
+    List<Integer> list = new ArrayList<>();
+    return list;
+  }
+
+  public int minSubArrayLen(int s, int[] nums) {
+    return 0;
   }
 }
--- algorithm/src/main/java/com/github/algorithm/array/StringAlgo.java
+++ algorithm/src/main/java/com/github/algorithm/array/StringAlgo.java
@@ -374,4 +374,18 @@ public class StringAlgo {
     }
     return Math.max(k, max);
   }
+
+  /**
+   * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
+   *
+   * 示例:
+   *
+   * 输入: s = 7, nums = [2,3,1,2,4,3]
+   * 输出: 2
+   * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+   */
+  public int minSubArrayLen(int s, int[] nums) {
+    return 0;
+  }
+
 }
