b1dba668357c60a71620004fb2e7a30b8bc2d236
吴晗 <han.wu@tongdun.cn>
2020-04-26 11:39:41 +0800

    update meta ans flavor

--- app/src/main/java/com/github/lotty/App.java
+++ app/src/main/java/com/github/lotty/App.java
@@ -1,6 +1,8 @@
 package com.github.lotty;
 
 import android.app.Application;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Choreographer;
 
@@ -15,17 +17,20 @@ public class App extends Application implements Choreographer.FrameCallback {
     //RootHooker.init(this);
 
     Choreographer choreographer = Choreographer.getInstance();
-    //choreographer.postFrameCallback(this);
+    choreographer.postFrameCallback(this);
+    try {
+      Bundle meta =
+          getPackageManager().getApplicationInfo(getPackageName(),
+              PackageManager.GET_META_DATA).metaData;
+      Log.e("wh", "meta:channel= " + meta.getString("channel"));
+      Log.e("wh", "meta:client= " + meta.getString("client"));
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override public void doFrame(long frameTimeNanos) {
-    Log.e("wh", "start sleep");
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    Log.e("wh", "stop sleep");
+    Log.e("wh", "do frame...");
   }
 }
 
